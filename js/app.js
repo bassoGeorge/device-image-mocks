ab46e9367d082f1ddbfd1279190bb3af573467a5
Anish George <anish.george@ignitesol.com>
2017-07-26 18:38:12 +0530

    Add thumbnail view

--- js/app.js
+++ js/app.js
@@ -3,31 +3,51 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 $(function(){
-    (window.devices || []).forEach(function(device){
-        var section = $("<section>", { class: "device" });
-        section.append("<h4>"+device.name+"</h4>");
+    var cPHeight = 600;
+    var rowCount = 0;
+    function makeNewRow(){
+        var row = $("<div>", { class: "row" });
+        $("#devices").append(row);
+        rowCount++;
+    };
+
+    // makeNewRow();
+    (window.devices || []).forEach(function(device, index){
+        var section = $("<section>", { class: "device four columns" });
+        section.append("<h5>"+device.name+"</h5>");
 
         var deviceRow = $("<div>");
 
         var portrait = $("<div>", { class: "mock mock-image-portrait" });
-        var landscape = $("<div>", { class: "mock mock-image-landscape" });
+        // var landscape = $("<div>", { class: "mock mock-image-landscape" });
+
+        var proportion = cPHeight / device.h;
+        var newH = cPHeight,
+            newW = device.w * proportion;
 
         portrait.css({
-            width: device.w,
-            height: device.h
+            width: newW,
+            height: newH
         });
 
-        landscape.css({
-            width: device.h,
-            height: device.w
+        portrait.data({
+            oh: device.h,
+            ow: device.w,
+            nh: newH,
+            nw: newW
         });
+
+        // landscape.css({
+        //     width: newH,
+        //     height: newW
+        // });
         deviceRow.append(portrait);
-        deviceRow.append(landscape);
+        // deviceRow.append(landscape);
 
         section.append(deviceRow);
         section.append($("<hr/>"));
-        $("#devices").append(section);
-
+        if (index % 3 == 0) makeNewRow();
+        $("#devices > .row:nth-child("+rowCount+")").append(section);
     });
 
     $("#portrait-toggle").click(function(){
@@ -36,6 +56,26 @@ $(function(){
 
     $("#landscape-toggle").click(function(){
         $(".mock-image-landscape").toggle();
+    });
+
+    $(".device").click(function(){
+        if ($(this).hasClass("focus")) {
+            $(".device").removeClass("focus-mode");
+            $(this).removeClass('focus');
+            var p = $(this).find(".mock");
+            p.css({
+                height: p.data("nh"),
+                width: p.data("nw")
+            });
+        } else {
+            $(".device").addClass("focus-mode");
+            $(this).addClass('focus');
+            var p = $(this).find(".mock");
+            p.css({
+                height: p.data("oh"),
+                width: p.data("ow")
+            });
+        }
     });
 
     $(".mock-image-landscape").hide();
